8ca8fb7d47a79cbfe3991cdda609878fbee1d307
MJLMills <mjohnmills@gmail.com>
2017-04-17 14:42:59 -0700

    new module for object functions

---
+++ ObjectFunctions.f90
@@ -0,0 +1,44 @@
+MODULE ObjectFunctions
+
+  IMPLICIT NONE
+
+  CONTAINS
+
+  REAL(8) FUNCTION objectFunction(variables)
+
+    ! #DES: This is the part that changes per-application. I am massively cheating by 
+    !       just importing eveything here for now.
+
+    USE Input, ONLY : dGTS, dGPS, mask, Nbins, minPop
+    USE Data, ONLY : energyGap, groundStateEnergy, mappingEnergies, RecomputeDependentData
+    USE Analysis, ONLY : FepUsFreeEnergies
+    USE FreeEnergy, ONLY : FepUS, ScanFepUS, Histogram, computeFEPProfile
+
+    IMPLICIT NONE
+
+    REAL(8), INTENT(IN) :: variables(:)
+    REAL(8) :: alpha(2), relative(3)
+    INTEGER :: binPopulations(Nbins,SIZE(energyGap,1)), binIndices(SIZE(energyGap,1),SIZE(energyGap,2))
+    REAL(8) :: binMidpoints(Nbins)
+    REAL(8) :: binGg(Nbins)
+    REAL(8) :: G_FEP(SIZE(energyGap,1))
+    LOGICAL :: printBin(Nbins)
+
+    ! For EVB-parameterisation, the variables are alpha and the parameters of the off-diagonals
+    ! and the object function must depend on the FEP/US free energy changes
+
+    ! compute the free energy values with the current parameter set
+    alpha(1) = 0.0d0; alpha(2) = variables(1)
+
+    CALL RecomputeDependentData(alpha)
+    CALL ComputeFEPProfile(1,SIZE(energyGap,1),mappingEnergies(:,:,:,1),mask(:,:),profile=G_FEP)
+    CALL Histogram(energyGap(:,:),mask,Nbins,binPopulations,binIndices,binMidpoints)
+    CALL FepUS(mappingEnergies(:,:,:,1),groundStateEnergy(:,:),G_FEP,binPopulations,binIndices,PMF1D=binGg,minPop=minPop,useBin=printBin)
+    CALL ScanFepUs(binMidpoints(:),binGg(:),mask=printBin,stationaryPoints=relative)
+
+    relative(:) = relative(:) - relative(1)
+    objectFunction = (relative(2) - dGTS)**2 + (relative(3) - dGPS)**2
+
+  END FUNCTION objectFunction
+
+END MODULE ObjectFunctions
